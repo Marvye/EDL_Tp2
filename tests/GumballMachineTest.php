a5a92557d19fc9c5dd0d057a6fe6e55f155033ae
Rosabelle LEKEMO ATONFACK <172558@S-L-IGLAB-12.umons.ac.be>
2019-11-20 10:10:29 +0100

    new3

--- tests/GumballMachineTest.php
+++ tests/GumballMachineTest.php
@@ -14,16 +14,21 @@ class GumballMachineTest extends PHPUnit_Framework_TestCase
 
 
     //prof
-        private $nom2="XXX2"; // a changer
-        private $prenom2="yyy2"; // a changer
-        private $date_naissance2="30-10-1981"; // a changer
-        private $lieu_naissance2="ZZZ2"; // a changer
+    private $nom2="XXX2"; // a changer
+    private $prenom2="yyy2"; // a changer
+    private $date_naissance2="30-10-1981"; // a changer
+    private $lieu_naissance2="ZZZ2"; // a changer
 
 
 
     // cours
     private $intitule="***"; //a remplir
     private $duree="***";    //a remplir
+
+    // cours
+        private $intitule1="IOT"; //a remplir
+        private $duree1="10";    //a remplir
+
     
         
     public function setUp()
@@ -38,9 +43,10 @@ class GumballMachineTest extends PHPUnit_Framework_TestCase
     public function testInsertP()
     {
         $max__id1=$this->gumballMachineInstance->GetLastIDP();
-        $this->assertEquals(true,$this->gumballMachineInstance->InsertP($this->gumballMachineInstance->getDB(),$this->nom2,$this->prenom2,$this->date_naissance2,$this->lieu_naissance2));
+        $this->assertEquals(true,$this->gumballMachineInstance->InsertP($this->gumballMachineInstance->getDB(),$this->nom,$this->prenom,$this->date_naissance,$this->lieu_naissance));
+        //his->assertEquals(true,$this->gumballMachineInstance->InsertP($this->gumballMachineInstance->getDB(),$this->nom2,$this->prenom2,$this->date_naissance2,$this->lieu_naissance2));
         $max__id2=$this->gumballMachineInstance->GetLastIDP();
-        $this->assertEquals($max__id1+2,$max__id2);
+        $this->assertEquals($max__id1+1,max__id2);
     }
     public function testAffichageProfAPI()
     {
