51540526326b8c1c501cf8f94f4c9cf6efcd6fe5
Rosabelle LEKEMO ATONFACK <172558@S-L-IGLAB-12.umons.ac.be>
2019-11-20 10:59:50 +0100

    new10

--- tests/GumballMachineTest.php
+++ tests/GumballMachineTest.php
@@ -68,7 +68,7 @@ class GumballMachineTest extends PHPUnit_Framework_TestCase
 
 
         $max__id1=$this->gumballMachineInstance->GetLastIDP();
-        $this->assertContains("good job",$this->gumballMachineInstance->InsertC($this->intitule1,$this->duree1,$this->gumballMachineInstance->GetIdP(nom2,prenom2)));
+        $this->assertContains("good job",$this->gumballMachineInstance->InsertC($this->intitule1,$this->duree1,$this->gumballMachineInstance->GetIdP($nom2,$prenom2)));
         $this->assertContains("good job",$this->gumballMachineInstance->InsertC($this->intitule2,$this->duree2,$this->gumballMachineInstance->GetIdP(nom,prenom)));
         //his->assertEquals(true,$this->gumballMachineInstance->InsertP($this->gumballMachineInstance->getDB(),$this->nom2,$this->prenom2,$this->date_naissance2,$this->lieu_naissance2));
         $max__id2=$this->gumballMachineInstance->GetLastIDP();
