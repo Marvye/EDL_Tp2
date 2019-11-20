a28738df6d35a020bf6ade5b5120a241ef7f3b29
Rosabelle LEKEMO ATONFACK <172558@S-L-IGLAB-12.umons.ac.be>
2019-11-20 10:46:46 +0100

    new7

--- tests/GumballMachineTest.php
+++ tests/GumballMachineTest.php
@@ -71,7 +71,7 @@ class GumballMachineTest extends PHPUnit_Framework_TestCase
         $this->assertContents("good job",$this->gumballMachineInstance->InsertC($this->intitule1,$this->duree1,$this->gumballMachineInstance->GetIdP(nom2,prenom2)));
         $this->assertContents("good job",$this->gumballMachineInstance->InsertC($this->intitule2,$this->duree2,$this->gumballMachineInstance->GetIdP(nom,prenom)));
         //his->assertEquals(true,$this->gumballMachineInstance->InsertP($this->gumballMachineInstance->getDB(),$this->nom2,$this->prenom2,$this->date_naissance2,$this->lieu_naissance2));
-        $max__id2=$this->gumballMachineInstance->GetLastIDP();
+        $max__id2=$this->gumballMachineInstance->GetLastIDC();
         $this->assertEquals($max__id1+1,max__id2);
     }
     public function testAffichageCoursAPI()
